1ec7c10cc9eb47164fa0be5e0773ca83d7cd4e69
Orange <bollwarm@tom.com>
2017-06-28 18:15:38 +0800

    fix baiduzhaopin 采集规则

--- example/baiduzhaopin.pl
+++ example/baiduzhaopin.pl
@@ -11,29 +11,39 @@ binmode( STDOUT, ':encoding(utf8)' );
 my ( $lurl, $re1, $re2 );
 my $language = shift;
 $language //= "perl";
-$lurl = 'http://zhaopin.baidu.com/?query=' . $language;
+$lurl = 'http://zhaopin.baidu.com/quanzhi?query=' . $language;
 $re1  = "div#feed-list";
-$re2  = "dl";
+$re2  = "a";
 my $pcontent = geturlcontent($lurl);
 
-my @tile        = split /\n/ms, getdiv( $pcontent, $re1, "dt a", 1 );
-my @source      = split /\n/ms, getdiv( $pcontent, $re1, "dd.jobs-source a" );
-my @time        = split /\n/ms, getdiv( $pcontent, $re1, "dd.jobs-time" );
-my @city        = split /\n/ms, getdiv( $pcontent, $re1, "dd.jobs-place" );
-my @company     = split /\n/ms, getdiv( $pcontent, $re1, "span.jobs-company" );
-my @salary      = split /\n/ms, getdiv( $pcontent, $re1, "p.feed-item-salary" );
-my @discription = split /\n/ms, getdiv( $pcontent, $re1, "p.detail-item" );
+my $urls=getdiv( $pcontent, $re1,"a",1),"\n";
+my @nurl=split /\n/ms,$urls;
+my @source;
+for(@nurl) {
+s/^\s+// if /szzw/;
+ push @source,"http://zhaopin.baidu.com$_ \n" if /szzw/;
+}
+
+my @tile        = split /\n/ms, getdiv( $pcontent, $re1, "a div.left div span");
+#print "@tile","\n";
+#my @source      = split /\n/ms, getdiv( $pcontent, $re1, "div.'right time' p" );
+my @time        = split /\n/ms, getdiv( $pcontent, $re1, "a div.right p" );
+my @city        = split /\n/ms, getdiv( $pcontent, $re1, "a div.title p" );
+my @company     = split /\n/ms, getdiv( $pcontent, $re1, "a div.left p" );
+my @salary      = split /\n/ms, getdiv( $pcontent, $re1, "a div.right p.salary" );
+#my @discription = split /\n/ms, getdiv( $pcontent, $re1, "a div.right p" );
 
 #print $pcout1;
 for ( 0 .. scalar(@tile) - 1 ) {
     print $tile[$_],    "\n";
-    print $source[$_],  "\n";
-    print $time[$_],    "\n";
+    print $source[$_]  ;
+    print $time[$_]    ;
     print $city[$_],    "\n";
     print $company[$_], "\n";
     print $salary[$_],  "\n";
-    print $discription[ 2 * $_ ], "\n";
-    print $discription[ 2 * $_ + 1 ], "\n\n";
+    print "\n=================\n";
+ #   print $discription[ 2 * $_ ], "\n";
+ #   print $discription[ 2 * $_ + 1 ], "\n\n";
 }
 print "get $lurl  ok \n";
 
